4154397ab6f5c69e82b79e0f354d193edb8d35f3
Ron Hartman <ronhartman@MacBook-Pro-van-Ron.local>
2026-04-15 21:56:57 +0200

    Fix Shell Recharge location API endpoint

--- shellrecharge/__init__.py
+++ shellrecharge/__init__.py
@@ -1,79 +1,67 @@
-"""The shellrecharge API code."""
+"""Shell Recharge API."""
 
-import logging
 from asyncio import CancelledError, TimeoutError
-from typing import Optional
+import logging
 
-import pydantic
-from aiohttp import ClientSession
-from aiohttp.client_exceptions import ClientError
+from aiohttp import ClientError, ClientSession
 from aiohttp_retry import ExponentialRetry, RetryClient
 from pydantic import ValidationError
+import pydantic
 from yarl import URL
 
-from .models import Location
-from .user import User
+from .error import LocationEmptyError, LocationValidationError, RateLimitHitError
+from .models.location import Location
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class Api:
-    """Class to make API requests."""
+    """API class."""
 
-    def __init__(self, websession: ClientSession):
-        """Initialize the session."""
+    def __init__(self, websession: ClientSession, logger: logging.Logger = _LOGGER) -> None:
+        """Initialize API class."""
         self.websession = websession
-        self.logger = logging.getLogger("shellrecharge")
+        self.logger = logger
 
     async def location_by_id(self, location_id: str) -> Location | None:
-        """
-        Perform API request.
-        Usually yields just one Location object with one or multiple chargers.
-        """
+        """Perform API request."""
         location = None
-
-	url = URL(
-	    "https://shellretaillocator.geoapp.me/api/v2/on_street_charger_locations/{}".format(
-		location_id
-	    )
-	).with_query(
-	    {
-		"locale": "nl_NL",
-		"format": "json",
-		"driving_distances": "false",
-	    }
-	)
+        url = URL(
+            "https://shellretaillocator.geoapp.me/api/v2/on_street_charger_locations/{}".format(
+                location_id
+            )
+        ).with_query(
+            {
+                "locale": "nl_NL",
+                "format": "json",
+                "driving_distances": "false",
+            }
+        )
         retry_client = RetryClient(
             client_session=self.websession,
             retry_options=ExponentialRetry(attempts=3, start_timeout=5),
         )
         try:
             async with retry_client.get(url) as response:
-		if response.status == 200:
-		    result = await response.json()
-		    if result:
-			if pydantic.version.VERSION.startswith("1"):
-			    location = Location.parse_obj(result)
-			else:
-			    location = Location.model_validate(result)
-		    else:
-			raise LocationEmptyError()
+                if response.status == 200:
+                    result = await response.json()
+                    if result:
+                        if pydantic.version.VERSION.startswith("1"):
+                            location = Location.parse_obj(result)
+                        else:
+                            location = Location.model_validate(result)
+                    else:
+                        raise LocationEmptyError()
                 elif response.status == 429:
                     raise RateLimitHitError("Rate limit of API has been hit")
                 else:
                     self.logger.exception(
-                        "HTTPError %s occurred while requesting %s",
-                        response.status,
-                        url,
+                        "HTTPError %s occurred while requesting %s", response.status, url
                     )
         except ValidationError as err:
             raise LocationValidationError(err)
-        except (
-            ClientError,
-            TimeoutError,
-            CancelledError,
-        ) as err:
-            # Something else failed
+        except (ClientError, TimeoutError, CancelledError) as err:
             raise err
-
         return location
 
     async def get_user(self, email: str, pwd: str, api_key: Optional[str] = None) -> User:
