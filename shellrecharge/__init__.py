d0e9585b84dc52e1bcd64b94025293b46cba80cf
Ron Hartman <ronhartman@MacBook-Pro-van-Ron.local>
2026-04-15 22:00:28 +0200

    Fix Shell Recharge location API endpoint

--- shellrecharge/__init__.py
+++ shellrecharge/__init__.py
@@ -1,30 +1,33 @@
-"""Shell Recharge API."""
+"""The shellrecharge API code."""
 
-from asyncio import CancelledError, TimeoutError
 import logging
+from asyncio import CancelledError, TimeoutError
+from typing import Optional
 
-from aiohttp import ClientError, ClientSession
+import pydantic
+from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientError
 from aiohttp_retry import ExponentialRetry, RetryClient
 from pydantic import ValidationError
-import pydantic
 from yarl import URL
 
-from .error import LocationEmptyError, LocationValidationError, RateLimitHitError
-from .models.location import Location
-
-_LOGGER = logging.getLogger(__name__)
+from .models import Location
+from .user import User
 
 
 class Api:
-    """API class."""
+    """Class to make API requests."""
 
-    def __init__(self, websession: ClientSession, logger: logging.Logger = _LOGGER) -> None:
-        """Initialize API class."""
+    def __init__(self, websession: ClientSession):
+        """Initialize the session."""
         self.websession = websession
-        self.logger = logger
+        self.logger = logging.getLogger("shellrecharge")
 
     async def location_by_id(self, location_id: str) -> Location | None:
-        """Perform API request."""
+        """Perform API request.
+
+        Usually yields just one Location object with one or multiple chargers.
+        """
         location = None
         url = URL(
             "https://shellretaillocator.geoapp.me/api/v2/on_street_charger_locations/{}".format(
@@ -37,10 +40,12 @@ class Api:
                 "driving_distances": "false",
             }
         )
+
         retry_client = RetryClient(
             client_session=self.websession,
             retry_options=ExponentialRetry(attempts=3, start_timeout=5),
         )
+
         try:
             async with retry_client.get(url) as response:
                 if response.status == 200:
@@ -56,34 +61,36 @@ class Api:
                     raise RateLimitHitError("Rate limit of API has been hit")
                 else:
                     self.logger.exception(
-                        "HTTPError %s occurred while requesting %s", response.status, url
+                        "HTTPError %s occurred while requesting %s",
+                        response.status,
+                        url,
                     )
         except ValidationError as err:
             raise LocationValidationError(err)
-        except (ClientError, TimeoutError, CancelledError) as err:
+        except (
+            ClientError,
+            TimeoutError,
+            CancelledError,
+        ) as err:
             raise err
+
         return location
 
     async def get_user(self, email: str, pwd: str, api_key: Optional[str] = None) -> User:
         user = User(email, pwd, self.websession, api_key)
         if not api_key:
             await user.authenticate()
+
         return user
 
 
 class LocationEmptyError(Exception):
     """Raised when returned Location API data is empty."""
 
-    pass
-
 
 class LocationValidationError(Exception):
     """Raised when returned Location API data is in the wrong format."""
 
-    pass
-
 
 class RateLimitHitError(Exception):
     """Raised when the rate limit of the API has been hit."""
-
-    pass
